17995d2ce170747225cf1327d969de3b413cb8dc
Francielle <franmkt2013@gmail.com>
2023-12-20 08:23:54 -0300

    Filter e forEach @FranhSilva

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,7 +1,11 @@
-const btnFiltrarLivrosDeFront = document.getElementById("btnFiltrarLivrosFront");
-btnFiltrarLivrosDeFront.addEventListener("click", filtrarLivros);
+const botoes = document.querySelectorAll(".btn"); //querySelector - filtra o primeiro elemento que encontrar... 
+                                                  //querySelectorAll - filtra todos os elementos que tiverem a mesma identificação informada.
+botoes.forEach(btn => btn.addEventListener("click", filtrarLivros));
 
 function filtrarLivros(){
-    let livrosFiltrados = livros.filter(livro => livro.categoria == "front-end");
+    const elementoBtn = document.getElementById(this.id); //aqui ele vai saber qual botão foi clicado.
+    const categoria = elementoBtn.value //aqui ele pega o valor do elemento referente ao botão que foi clicado.
+
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
     console.table(livrosFiltrados);
 }
