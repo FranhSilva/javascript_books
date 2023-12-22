763873af54ebe315367ba7cff09b4deffab1ac8e
Francielle <franmkt2013@gmail.com>
2023-12-22 08:40:31 -0300

    disponibilidade de livros com Operador Terciário @FranhSilva

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,7 +3,8 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros){
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach(livro => {
-    let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+    // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+    let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
     elementoParaInserirLivros.innerHTML += `
       <div class="livro">
       <img class="${disponibilidade}" src="${livro.imagem}"
@@ -21,10 +22,10 @@ function exibirOsLivrosNaTela(listaDeLivros){
   })
 }
 
-function verificarDisponibilidadeDoLivro(livro){
-  if (livro.quantidade > 0){
-    return "livro__imagens";
-  } else {
-    return "livro__imagens indisponivel";
-  }
-}
+// function verificarDisponibilidadeDoLivro(livro){
+//   if (livro.quantidade > 0){
+//     return "livro__imagens";
+//   } else {
+//     return "livro__imagens indisponivel";
+//   }
+// }
